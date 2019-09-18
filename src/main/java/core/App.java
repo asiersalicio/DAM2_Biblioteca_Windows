4c19566531f201fe997770413e81369078af9f03
unknown <Elorrieta Errekamari@INFP4A101-21.H015112.NET>
2019-09-18 13:12:32 +0200

    Filechoosser

--- src/main/java/core/App.java
+++ src/main/java/core/App.java
@@ -2,20 +2,27 @@ package core;
 
 import java.util.Scanner;
 
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import io.FileReader;
 import io.TerminalReader;
 
 public class App {
 
 	public static TerminalReader terminalReader;
+	public static FileReader fileReader;
 	
 	public static void main(String[] args) {
 		Inicializar();
 		System.out.println(terminalReader.nextInt("AAAA", 0, 10));
+		fileReader.ElegirArchivo(new FileNameExtensionFilter("CSV", ".csv", ".txt"));
 	}
 	
 	private static void Inicializar()
 	{
 		terminalReader = new TerminalReader(new Scanner(System.in));
+		fileReader = new FileReader(new JFileChooser());
 	}
 
 }
